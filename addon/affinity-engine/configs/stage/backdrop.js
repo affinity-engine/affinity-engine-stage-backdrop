f0499593069a5cf8e5e3ca228afa7229cb233e2f
null null null <null@affinitygamescollective.org>
2017-09-13 13:20:16 -0400

    prevent default config conflicts with user config

--- addon/affinity-engine/configs/stage/backdrop.js
+++ addon/affinity-engine/configs/stage/backdrop.js
@@ -1,12 +1,14 @@
 export default {
   priority: 3,
-  component: {
-    stage: {
-      direction: {
-        backdrop: {
-          attrs: {
-            layer: 'stage.image.background',
-            renderMethod: 'cover'
+  default: {
+    component: {
+      stage: {
+        direction: {
+          backdrop: {
+            attrs: {
+              layer: 'stage.image.background',
+              renderMethod: 'cover'
+            }
           }
         }
       }
